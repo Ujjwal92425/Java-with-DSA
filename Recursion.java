0d504da4b207f7ce825c4322ffe83fdbb21e2c9f
Ujjwal Kumar <ujjwalsps54@gmail.com>
2025-02-25 11:01:56 +0530

    Count x in a string

--- Recursion.java
+++ Recursion.java
@@ -97,6 +97,55 @@ public class Recursion {
         //     return count+count7(n/10);
         //   }
          */
+
+         //^Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit, except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+        //! count8(8) → 1   count8(818) → 2 count8(8818) → 4
+
+        /* public int count8(int n) {
+        //     if(n==0) {
+        //       return 0;
+        //     }
+        //     int count = 0;
+        //     if(n%10==8) {
+        //       count=1;
+              
+        //       if((n/10) %10 ==8) {
+        //         count = 2;
+        //       }
+        //     }
+        //     return count+count8(n/10);
+        //   }
+        */
+
+        //^ Power 
+        /* public int powerN(int base, int n) {
+            if(n==1) {
+              return base;
+            }
+            if(n==0) {
+              return 1;
+            }
+            return base* powerN(base,n-1);
+          }
+            */
+            
+        //^Count x in a string
+        /* 
+        public int countX(String str) {
+          int count =0;
+          if(str.length()==0) {
+            return 0;
+          }
+          if(str.charAt(str.length()-1) == 'x') {
+            count++;
+          }
+          return count + countX(str.substring(0,str.length()-1));
+        }
+          */
+        
+        
+          
+
           
 
     }
